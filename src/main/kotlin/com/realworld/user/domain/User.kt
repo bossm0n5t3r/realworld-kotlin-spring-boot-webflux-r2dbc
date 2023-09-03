11a0ea2b6e02eee4ab10a53a85f58f65b0ce50eb
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-09-03 14:31:41 +0900

    feat: Add User

---
+++ src/main/kotlin/com/realworld/user/domain/User.kt
@@ -0,0 +1,31 @@
+package com.realworld.user.domain
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+
+@Suppress("LongParameterList")
+@Table("user_info")
+data class User(
+    @Id
+    val id: Long? = null,
+    var username: String,
+    var email: String,
+    var encodedPassword: String,
+    var bio: String? = null,
+    var image: String? = null,
+    var followingIds: String? = null,
+    var favoriteArticlesIds: String? = null,
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as User
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
