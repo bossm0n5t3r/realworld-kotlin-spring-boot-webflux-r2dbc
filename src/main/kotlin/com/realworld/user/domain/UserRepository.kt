4abcf1fb4c6db7b0fb2bf269078591297b6ed404
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-09-06 23:00:00 +0900

    fix: Edit UserRepository
    - Rename ReactiveCrudRepository to R2dbcRepository

--- src/main/kotlin/com/realworld/user/domain/UserRepository.kt
+++ src/main/kotlin/com/realworld/user/domain/UserRepository.kt
@@ -1,11 +1,11 @@
 package com.realworld.user.domain
 
-import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import org.springframework.data.r2dbc.repository.R2dbcRepository
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Mono
 
 @Repository
-interface UserRepository : ReactiveCrudRepository<User, Long> {
+interface UserRepository : R2dbcRepository<User, Long> {
     fun existsByEmail(email: String): Mono<Boolean>
     fun existsByUsername(username: String): Mono<Boolean>
 }
