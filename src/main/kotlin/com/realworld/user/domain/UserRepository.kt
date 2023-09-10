4c798de3369cad6a2de3e92effd637f3488a65bf
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-09-11 00:24:00 +0900

    fix: Edit UserRepository

--- src/main/kotlin/com/realworld/user/domain/UserRepository.kt
+++ src/main/kotlin/com/realworld/user/domain/UserRepository.kt
@@ -2,10 +2,12 @@ package com.realworld.user.domain
 
 import org.springframework.data.r2dbc.repository.R2dbcRepository
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @Repository
 interface UserRepository : R2dbcRepository<User, Long> {
     fun existsByEmail(email: String): Mono<Boolean>
     fun existsByUsername(username: String): Mono<Boolean>
+    fun findAllByEmail(email: String): Flux<User>
 }
