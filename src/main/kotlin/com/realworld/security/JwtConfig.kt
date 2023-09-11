8d7a771fc70e8c0ab796628dacc09449f6717734
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-09-12 02:39:20 +0900

    fix: Edit JwtConfig
    - Edit jwtServerAuthenticationConverter
      - Make functional
    - Edit getAuthorizationHeader
    - Edit jwtAuthManager
      - Make functional

--- src/main/kotlin/com/realworld/security/JwtConfig.kt
+++ src/main/kotlin/com/realworld/security/JwtConfig.kt
@@ -1,42 +1,53 @@
 package com.realworld.security
 
+import com.realworld.exception.InvalidRequestException
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpHeaders
 import org.springframework.security.authentication.ReactiveAuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.web.server.authentication.AuthenticationWebFilter
 import org.springframework.security.web.server.authentication.ServerAuthenticationConverter
 import org.springframework.web.server.ServerWebExchange
-import reactor.kotlin.core.publisher.toMono
+import reactor.core.publisher.Mono
 
 @Configuration
 class JwtConfig {
     @Bean
     fun jwtServerAuthenticationConverter(tokenFormatter: TokenFormatter): ServerAuthenticationConverter =
         ServerAuthenticationConverter { exchange ->
-            val authorizationHeader = getAuthorizationHeader(exchange)
-            authorizationHeader?.let {
-                val token = tokenFormatter.getRowToken(it)
-                UsernamePasswordAuthenticationToken(token, token)
-            }.toMono()
+            Mono.justOrEmpty(getAuthorizationHeader(exchange))
+                .map {
+                    val token = tokenFormatter.getRowToken(it)
+                    UsernamePasswordAuthenticationToken(token, token)
+                }
         }
 
     private fun getAuthorizationHeader(exchange: ServerWebExchange): String? {
         val headers = exchange.request.headers[HttpHeaders.AUTHORIZATION] ?: emptyList()
-        return headers.firstOrNull(String::isNotEmpty)
+        return headers.firstOrNull { it.isNotEmpty() }
     }
 
     @Bean
     fun jwtAuthManager(signer: UserTokenProvider): ReactiveAuthenticationManager =
         ReactiveAuthenticationManager { authentication ->
-            val token = authentication.credentials as String
-            val jws = signer.validate(token)
-            val authority = SimpleGrantedAuthority("ROLE_USER")
-            val userId = jws.subject
-            val tokenPrincipal = TokenPrincipal(userId, token)
-            UsernamePasswordAuthenticationToken(tokenPrincipal, token, listOf(authority)).toMono()
+            Mono.justOrEmpty(authentication)
+                .map { it.credentials as String }
+                .handle { token, sink ->
+                    val jws = signer.validate(token)
+                    if (jws == null) {
+                        sink.error(InvalidRequestException("Token", "invalid"))
+                        return@handle
+                    }
+                    val authority = SimpleGrantedAuthority("ROLE_USER")
+                    val userId = jws.subject
+                    val tokenPrincipal = TokenPrincipal(userId, token)
+                    sink.next(UsernamePasswordAuthenticationToken(tokenPrincipal, token, listOf(authority)))
+                }
+                .doOnError { throw it }
+                .map { it as Authentication }
         }
 
     @Bean
