0f0c4628945a2c0b6ee8f8ef30be5511f2fa5cb1
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-09-13 01:10:00 +0900

    fix: Edit UserRepository
    - Add findAllByUsername

--- src/main/kotlin/com/realworld/user/domain/UserRepository.kt
+++ src/main/kotlin/com/realworld/user/domain/UserRepository.kt
@@ -10,5 +10,6 @@ interface UserRepository : R2dbcRepository<User, Long> {
     fun existsByEmail(email: String): Mono<Boolean>
     fun existsByUsername(username: String): Mono<Boolean>
     fun findAllByEmail(email: String): Flux<User>
+    fun findAllByUsername(username: String): Flux<User>
     fun findAllById(id: Long): Flux<User>
 }
