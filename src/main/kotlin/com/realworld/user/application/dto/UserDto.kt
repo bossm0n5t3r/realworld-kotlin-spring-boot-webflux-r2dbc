6cd111c07fbb54cbbecc9b05bc203b3bb89ee008
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-09-15 04:12:10 +0900

    feat: Add UserDto

---
+++ src/main/kotlin/com/realworld/user/application/dto/UserDto.kt
@@ -0,0 +1,65 @@
+package com.realworld.user.application.dto
+
+import com.realworld.user.presentation.dto.Profile
+import com.realworld.user.domain.User
+import com.realworld.user.presentation.dto.AuthenticationUser
+
+data class UserDto(
+    val id: Long? = null,
+    val username: String,
+    val email: String,
+    val encodedPassword: String,
+    val bio: String? = null,
+    val image: String? = null,
+    val followingIdList: MutableSet<Long> = mutableSetOf(),
+    val favoriteArticlesIdList: MutableSet<Long> = mutableSetOf(),
+) {
+    companion object {
+        fun User.toDto() = UserDto(
+            id = this.id,
+            username = this.username,
+            email = this.email,
+            encodedPassword = this.encodedPassword,
+            bio = this.bio,
+            image = this.image,
+            followingIdList = this.followingIds
+                ?.takeIf { it.isNotBlank() }
+                ?.split(",")
+                ?.map { it.toLong() }
+                ?.toMutableSet()
+                ?: mutableSetOf(),
+            favoriteArticlesIdList = this.favoriteArticlesIds
+                ?.takeIf { it.isNotBlank() }
+                ?.split(",")
+                ?.map { it.toLong() }
+                ?.toMutableSet()
+                ?: mutableSetOf(),
+        )
+
+        fun UserDto.toEntity() = User(
+            id = this.id,
+            username = this.username,
+            email = this.email,
+            encodedPassword = this.encodedPassword,
+            bio = this.bio,
+            image = this.image,
+            followingIds = this.followingIdList.joinToString(","),
+            favoriteArticlesIds = this.favoriteArticlesIdList.joinToString(","),
+        )
+
+        fun UserDto.toAuthenticationUser(token: String) = AuthenticationUser(
+            email = this.email,
+            token = token,
+            username = this.username,
+            bio = this.bio,
+            image = this.image,
+        )
+
+        fun UserDto.toProfile(following: Boolean) = Profile(
+            username = this.username,
+            bio = this.bio,
+            image = this.image,
+            following = following,
+        )
+    }
+}
