ba2aa77449eb0e9ef1a7c7cc4bbf7501f6b5e255
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-09-15 04:41:41 +0900

    fix: Edit UserController
    - Convert dtos when entering and exiting UserService

--- src/main/kotlin/com/realworld/user/presentation/UserController.kt
+++ src/main/kotlin/com/realworld/user/presentation/UserController.kt
@@ -1,9 +1,16 @@
 package com.realworld.user.presentation
 
-import com.realworld.profile.dto.Profile
-import com.realworld.profile.dto.ProfileWrapper
 import com.realworld.user.application.UserService
+import com.realworld.user.application.dto.SignInDto.Companion.toSignInDto
+import com.realworld.user.application.dto.SignUpDto.Companion.toSignUpDto
+import com.realworld.user.application.dto.UpdateUserDto.Companion.toUpdateUserDto
+import com.realworld.user.application.dto.UserDto.Companion.toAuthenticationUser
+import com.realworld.user.application.dto.UserDto.Companion.toProfile
 import com.realworld.user.presentation.dto.AuthenticationUser
+import com.realworld.user.presentation.dto.AuthenticationUser.Companion.withUserWrapper
+import com.realworld.user.presentation.dto.Profile
+import com.realworld.user.presentation.dto.Profile.Companion.withProfileWrapper
+import com.realworld.user.presentation.dto.ProfileWrapper
 import com.realworld.user.presentation.dto.SignInRequest
 import com.realworld.user.presentation.dto.SignUpRequest
 import com.realworld.user.presentation.dto.UpdateRequest
@@ -24,20 +31,23 @@ class UserController(
     fun signUp(
         @RequestBody signUpRequest: Mono<UserWrapper<SignUpRequest>>,
     ): Mono<UserWrapper<AuthenticationUser>> {
-        return userService.signUp(signUpRequest)
+        return userService.signUp(signUpRequest.map { it.user.toSignUpDto() })
+            .map { (userDto, token) -> userDto.toAuthenticationUser(token).withUserWrapper() }
     }
 
     @PostMapping("/api/users/login")
     fun signIn(
         @RequestBody signInRequest: Mono<UserWrapper<SignInRequest>>,
     ): Mono<UserWrapper<AuthenticationUser>> {
-        return userService.signIn(signInRequest)
+        return userService.signIn(signInRequest.map { it.user.toSignInDto() })
+            .map { (userDto, token) -> userDto.toAuthenticationUser(token).withUserWrapper() }
     }
 
     @GetMapping("/api/user")
     fun getUser(): Mono<UserWrapper<AuthenticationUser>> {
         // FIXME Handle error response when throw InvalidJwtException
         return userService.getUser()
+            .map { it.userDto.toAuthenticationUser(it.token).withUserWrapper() }
     }
 
     @PutMapping("/api/user")
@@ -45,12 +55,14 @@ class UserController(
         @RequestBody updateRequest: Mono<UserWrapper<UpdateRequest>>,
     ): Mono<UserWrapper<AuthenticationUser>> {
         // FIXME Handle error response when throw InvalidJwtException
-        return userService.update(updateRequest)
+        return userService.update(updateRequest.map { it.user.toUpdateUserDto() })
+            .map { (userDto, token) -> userDto.toAuthenticationUser(token).withUserWrapper() }
     }
 
     @GetMapping("/api/profiles/{username}")
     fun getProfile(@PathVariable username: String): Mono<ProfileWrapper<Profile>> {
         // FIXME Handle error response when throw InvalidJwtException
         return userService.getProfile(username)
+            .map { (userDto, following) -> userDto.toProfile(following).withProfileWrapper() }
     }
 }
