8182bb1756bead0a1a5770c69ac68b7b6305d1af
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-09-16 04:20:40 +0900

    feat: Add ErrorCode

---
+++ src/main/kotlin/com/realworld/exception/ErrorCode.kt
@@ -0,0 +1,13 @@
+package com.realworld.exception
+
+enum class ErrorCode(
+    val target: ErrorTarget,
+    val action: ErrorAction,
+) {
+    EMAIL_ALREADY_EXISTS(ErrorTarget.EMAIL, ErrorAction.ALREADY_EXISTS),
+    USERNAME_ALREADY_EXISTS(ErrorTarget.USERNAME, ErrorAction.ALREADY_EXISTS),
+    USER_NOT_FOUND(ErrorTarget.USER, ErrorAction.NOT_FOUND),
+    USERNAME_NOT_FOUND(ErrorTarget.USERNAME, ErrorAction.NOT_FOUND),
+    TOKEN_INVALID(ErrorTarget.TOKEN, ErrorAction.INVALID),
+    AUTHORIZATION_HEADER_HAS_NO_TOKEN_PREFIX(ErrorTarget.AUTHORIZATION_HEADER, ErrorAction.HAS_NO_TOKEN_PREFIX),
+}
