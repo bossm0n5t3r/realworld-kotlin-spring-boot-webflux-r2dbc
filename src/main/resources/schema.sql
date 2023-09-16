5192b9ae3f9d98abb21985eefde019b43be1647c
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-09-16 23:10:03 +0900

    fix: Edit schema.sql
    - Edit TABLE user_info
    - Edit TABLE meta_followee_follower

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,22 @@
-CREATE TABLE user_info (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT ,
-    username VARCHAR(100) NOT NULL ,
-    email VARCHAR(100) NOT NULL ,
-    encoded_password VARCHAR(100) NOT NULL ,
+CREATE TABLE user_info
+(
+    id               BIGINT PRIMARY KEY AUTO_INCREMENT,
+    created_at       DATETIME,
+    updated_at       DATETIME,
+    username         VARCHAR(100) NOT NULL,
+    email            VARCHAR(100) NOT NULL,
+    encoded_password VARCHAR(100) NOT NULL,
     bio VARCHAR(200),
     image VARCHAR(200),
-    following_ids CLOB,
-    favorite_articles_ids CLOB,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE meta_followee_follower
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    created_at DATETIME,
+    updated_at DATETIME,
+    followee_user_id BIGINT NOT NULL,
+    follower_user_id BIGINT NOT NULL,
     PRIMARY KEY (id)
 );
