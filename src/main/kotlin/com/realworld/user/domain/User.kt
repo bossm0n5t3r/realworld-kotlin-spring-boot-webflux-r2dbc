bb378e098dbf07bb38cda7aed24c9e27a575de75
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-09-16 23:11:00 +0900

    fix: Edit User
    - Add createdAt
    - Add updatedAt
    - Delete followingIds
    - Delete favoriteArticlesIds

--- src/main/kotlin/com/realworld/user/domain/User.kt
+++ src/main/kotlin/com/realworld/user/domain/User.kt
@@ -1,20 +1,25 @@
 package com.realworld.user.domain
 
+import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.relational.core.mapping.Table
+import java.time.Instant
 
 @Suppress("LongParameterList")
 @Table("user_info")
 data class User(
     @Id
     val id: Long? = null,
+    @CreatedDate
+    var createdAt: Instant,
+    @LastModifiedDate
+    var updatedAt: Instant,
     var username: String,
     var email: String,
     var encodedPassword: String,
     var bio: String? = null,
     var image: String? = null,
-    var followingIds: String? = null,
-    var favoriteArticlesIds: String? = null,
 ) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
