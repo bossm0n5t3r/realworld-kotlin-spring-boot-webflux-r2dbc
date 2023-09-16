275724e79798930e4a8b1bc0d9c2bda09fa0da62
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-09-16 23:11:20 +0900

    fix: Edit UserDto
    - Add createdAt
    - Add updatedAt
    - Delete followingIds
    - Delete favoriteArticlesIds

--- src/main/kotlin/com/realworld/user/application/dto/UserDto.kt
+++ src/main/kotlin/com/realworld/user/application/dto/UserDto.kt
@@ -3,66 +3,39 @@ package com.realworld.user.application.dto
 import com.realworld.user.domain.User
 import com.realworld.user.presentation.dto.AuthenticationUser
 import com.realworld.user.presentation.dto.Profile
+import java.time.Instant
 
 data class UserDto(
     val id: Long? = null,
+    val createdAt: Instant = Instant.now(),
+    val updatedAt: Instant = Instant.now(),
     val username: String,
     val email: String,
     val encodedPassword: String,
     val bio: String? = null,
     val image: String? = null,
-    val followingIdList: MutableSet<Long> = mutableSetOf(),
-    val favoriteArticlesIdList: MutableSet<Long> = mutableSetOf(),
 ) {
-    fun follow(userIdToFollow: Long?): Boolean {
-        return if (userIdToFollow != null) {
-            followingIdList.add(userIdToFollow)
-            true
-        } else {
-            false
-        }
-    }
-
-    fun unfollow(userIdToFollow: Long?): Boolean {
-        return if (userIdToFollow != null) {
-            followingIdList.remove(userIdToFollow)
-            true
-        } else {
-            false
-        }
-    }
-
     companion object {
         fun User.toDto() = UserDto(
             id = this.id,
+            createdAt = this.createdAt,
+            updatedAt = this.updatedAt,
             username = this.username,
             email = this.email,
             encodedPassword = this.encodedPassword,
             bio = this.bio,
             image = this.image,
-            followingIdList = this.followingIds
-                ?.takeIf { it.isNotBlank() }
-                ?.split(",")
-                ?.map { it.toLong() }
-                ?.toMutableSet()
-                ?: mutableSetOf(),
-            favoriteArticlesIdList = this.favoriteArticlesIds
-                ?.takeIf { it.isNotBlank() }
-                ?.split(",")
-                ?.map { it.toLong() }
-                ?.toMutableSet()
-                ?: mutableSetOf(),
         )
 
         fun UserDto.toEntity() = User(
             id = this.id,
+            createdAt = this.createdAt,
+            updatedAt = this.updatedAt,
             username = this.username,
             email = this.email,
             encodedPassword = this.encodedPassword,
             bio = this.bio,
             image = this.image,
-            followingIds = this.followingIdList.joinToString(","),
-            favoriteArticlesIds = this.favoriteArticlesIdList.joinToString(","),
         )
 
         fun UserDto.toAuthenticationUser(token: String) = AuthenticationUser(
