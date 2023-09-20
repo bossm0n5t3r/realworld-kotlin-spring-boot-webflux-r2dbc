b743f7a643a17be765191afeea1c0f08196e0995
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-09-20 23:40:00 +0900

    fix: Edit Profile
    - Set default values for the properties

--- src/main/kotlin/com/realworld/user/presentation/dto/Profile.kt
+++ src/main/kotlin/com/realworld/user/presentation/dto/Profile.kt
@@ -1,10 +1,10 @@
 package com.realworld.user.presentation.dto
 
 data class Profile(
-    val username: String,
-    val bio: String?,
-    val image: String?,
-    val following: Boolean,
+    val username: String = "",
+    val bio: String? = null,
+    val image: String? = null,
+    val following: Boolean = false,
 ) {
     fun withProfileWrapper() = ProfileWrapper(this)
 }
