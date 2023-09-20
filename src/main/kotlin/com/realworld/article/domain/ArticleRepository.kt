126c0c2054bb812d64a4d1ebcb6490ebea943d22
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-09-20 23:30:00 +0900

    feat: Add ArticleRepository

---
+++ src/main/kotlin/com/realworld/article/domain/ArticleRepository.kt
@@ -0,0 +1,7 @@
+package com.realworld.article.domain
+
+import org.springframework.data.r2dbc.repository.R2dbcRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ArticleRepository : R2dbcRepository<Article, Long>
