3969307e20808444e41d92e9ec5c5223477c74d6
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-09-25 05:09:01 +0900

    feat: Add MetaUserFavoriteArticleRepository

---
+++ src/main/kotlin/com/realworld/meta/domain/MetaUserFavoriteArticleRepository.kt
@@ -0,0 +1,10 @@
+package com.realworld.meta.domain
+
+import org.springframework.data.r2dbc.repository.R2dbcRepository
+import org.springframework.stereotype.Repository
+import reactor.core.publisher.Flux
+
+@Repository
+interface MetaUserFavoriteArticleRepository : R2dbcRepository<MetaUserFavoriteArticle, Long> {
+    fun findAllByUserId(userId: Long): Flux<MetaUserFavoriteArticle>
+}
