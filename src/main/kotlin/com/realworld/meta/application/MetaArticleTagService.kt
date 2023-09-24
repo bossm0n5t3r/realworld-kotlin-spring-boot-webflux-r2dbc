8ba0a6ea209a6c50f13193383aa172e55064d0fb
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-09-25 05:10:51 +0900

    fix: Edit MetaArticleTagService
    - Add getArticleIdsFromTagName

--- src/main/kotlin/com/realworld/meta/application/MetaArticleTagService.kt
+++ src/main/kotlin/com/realworld/meta/application/MetaArticleTagService.kt
@@ -2,13 +2,28 @@ package com.realworld.meta.application
 
 import com.realworld.meta.domain.MetaArticleTag
 import com.realworld.meta.domain.MetaArticleTagRepository
+import com.realworld.tag.domain.TagRepository
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
+import java.util.Optional
 
 @Service
 class MetaArticleTagService(
+    private val tagRepository: TagRepository,
     private val metaArticleTagRepository: MetaArticleTagRepository,
 ) {
+    fun getArticleIdsFromTagName(tagName: String?): Optional<Set<Long>> {
+        if (tagName == null) return Optional.empty()
+        return tagRepository.findAllByName(tagName)
+            .collectList()
+            .map { tagList -> tagList.mapNotNull { it.id }.toSet() }
+            .flatMap { tagIdSet ->
+                metaArticleTagRepository.findAllByTagIdIn(tagIdSet).map { it.articleId }.collectList()
+            }
+            .map { it.toSet() }
+            .blockOptional()
+    }
+
     fun saveArticleIdToTagIds(articleId: Long, tagIds: Flux<Long>) {
         tagIds.subscribe {
             metaArticleTagRepository.save(MetaArticleTag(articleId = articleId, tagId = it))
