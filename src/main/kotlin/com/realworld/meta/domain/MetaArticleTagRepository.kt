0f5885b6d6a5805f1bed9aab58d6ecc315192de5
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-09-25 05:07:40 +0900

    fix: Edit MetaArticleTagRepository
    - Add findAllByTagIdIn

--- src/main/kotlin/com/realworld/meta/domain/MetaArticleTagRepository.kt
+++ src/main/kotlin/com/realworld/meta/domain/MetaArticleTagRepository.kt
@@ -2,6 +2,9 @@ package com.realworld.meta.domain
 
 import org.springframework.data.r2dbc.repository.R2dbcRepository
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Flux
 
 @Repository
-interface MetaArticleTagRepository : R2dbcRepository<MetaArticleTag, Long>
+interface MetaArticleTagRepository : R2dbcRepository<MetaArticleTag, Long> {
+    fun findAllByTagIdIn(tagIds: Collection<Long>): Flux<MetaArticleTag>
+}
