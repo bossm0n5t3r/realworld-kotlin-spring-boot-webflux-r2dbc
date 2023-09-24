f84f89aec6329d4c791a12cb0caf61245355e7e7
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-09-25 05:17:05 +0900

    fix: Edit ArticleService
    - Add getArticles

--- src/main/kotlin/com/realworld/article/application/ArticleService.kt
+++ src/main/kotlin/com/realworld/article/application/ArticleService.kt
@@ -4,23 +4,33 @@ import com.realworld.article.application.dto.ArticleDto
 import com.realworld.article.application.dto.ArticleDto.Companion.toDto
 import com.realworld.article.application.dto.CreateArticleDto
 import com.realworld.article.domain.ArticleRepository
+import com.realworld.article.domain.ArticleTemplateRepository
+import com.realworld.article.presentation.dto.Article
+import com.realworld.article.presentation.dto.ArticlesWrapper
 import com.realworld.exception.ErrorCode
 import com.realworld.exception.InvalidRequestException
 import com.realworld.meta.application.MetaArticleTagService
 import com.realworld.meta.application.MetaFolloweeFollowerService
+import com.realworld.meta.application.MetaUserFavoriteArticleService
 import com.realworld.security.UserSessionProvider
 import com.realworld.tag.application.TagService
+import com.realworld.user.application.UserService
 import com.realworld.user.application.dto.UserDto
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
+import reactor.core.scheduler.Schedulers
 import reactor.kotlin.core.publisher.toFlux
+import kotlin.jvm.optionals.getOrNull
 
 @Service
 class ArticleService(
     private val userSessionProvider: UserSessionProvider,
+    private val userService: UserService,
     private val articleRepository: ArticleRepository,
     private val tagService: TagService,
+    private val articleTemplateRepository: ArticleTemplateRepository,
     private val metaFolloweeFollowerService: MetaFolloweeFollowerService,
+    private val metaUserFavoriteArticleService: MetaUserFavoriteArticleService,
     private val metaArticleTagService: MetaArticleTagService,
 ) {
     fun createArticle(request: Mono<CreateArticleDto>): Mono<Triple<ArticleDto, UserDto, Boolean>> {
@@ -51,5 +61,55 @@ class ArticleService(
                         }
                     }
             }
+    }
+
+    fun getArticles(
+        tag: String?,
+        author: String?,
+        favoritedByUser: String?,
+        limit: Int,
+        offset: Int,
+    ): Mono<ArticlesWrapper<Article>> {
+        return Mono
+            .zip(
+                userSessionProvider.getCurrentUserSession()
+                    .flatMap { metaFolloweeFollowerService.getFollowingIds(it.userDto.id).collectList() }
+                    .switchIfEmpty(Mono.just(emptyList())),
+                Mono.just(userService.getUserDtoFromUsername(author)),
+                Mono.just(userService.getUserDtoFromUsername(favoritedByUser)),
+            )
+            .flatMap {
+                val followingIdSet = it.t1.toSet()
+                val authorUserDto = it.t2.getOrNull()
+                val favoritedUserId = it.t3.getOrNull()?.id
+                val filteredIds = getFilteredArticleIds(tag, favoritedUserId)
+
+                articleTemplateRepository.findNewestArticlesFilteredBy(
+                    filteredIds = filteredIds,
+                    authorId = authorUserDto?.id,
+                    limit = limit,
+                    offset = offset.toLong(),
+                )
+                    .publishOn(Schedulers.boundedElastic())
+                    .map { article ->
+                        val articleAuthorUserDto = authorUserDto
+                            ?: userService.getUserDtoFromUserId(article.authorId).getOrNull()
+                        val profile = articleAuthorUserDto
+                            ?.toProfile(following = followingIdSet.contains(articleAuthorUserDto.id))
+                        article.toDto().toArticle(profile)
+                    }
+                    .collectList()
+                    .map { articles -> ArticlesWrapper(articles = articles, articlesCount = articles.size) }
+            }
+    }
+
+    private fun getFilteredArticleIds(tag: String?, favoritedUserId: Long?): Set<Long> {
+        val favoriteArticleIdSet = metaUserFavoriteArticleService.getFavoriteArticleIds(favoritedUserId)
+            .getOrNull()
+            ?: emptySet()
+        val articleIdSetWithTag = metaArticleTagService.getArticleIdsFromTagName(tag)
+            .getOrNull()
+            ?: emptySet()
+        return favoriteArticleIdSet.intersect(articleIdSetWithTag)
     }
 }
