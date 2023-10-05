195c200649d6c384f09fbe4d6d9d491246c0d3ef
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-10-06 01:30:00 +0900

    fix: Edit ArticleRepository
    - Add findAllByAuthorIdIn

--- src/main/kotlin/com/realworld/article/domain/ArticleRepository.kt
+++ src/main/kotlin/com/realworld/article/domain/ArticleRepository.kt
@@ -1,7 +1,11 @@
 package com.realworld.article.domain
 
+import org.springframework.data.domain.Pageable
 import org.springframework.data.r2dbc.repository.R2dbcRepository
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Flux
 
 @Repository
-interface ArticleRepository : R2dbcRepository<ArticleEntity, Long>
+interface ArticleRepository : R2dbcRepository<ArticleEntity, Long> {
+    fun findAllByAuthorIdIn(authorIds: Collection<Long>, pageable: Pageable): Flux<ArticleEntity>
+}
