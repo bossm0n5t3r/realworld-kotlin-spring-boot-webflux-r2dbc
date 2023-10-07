fedb9dba9a8acc48c935f8782d8ce5921a95948b
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-10-07 14:01:01 +0900

    fix: Edit ArticleRepository
    - Add findBySlug

--- src/main/kotlin/com/realworld/article/domain/ArticleRepository.kt
+++ src/main/kotlin/com/realworld/article/domain/ArticleRepository.kt
@@ -4,8 +4,10 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.r2dbc.repository.R2dbcRepository
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @Repository
 interface ArticleRepository : R2dbcRepository<ArticleEntity, Long> {
     fun findAllByAuthorIdIn(authorIds: Collection<Long>, pageable: Pageable): Flux<ArticleEntity>
+    fun findBySlug(slug: String): Mono<ArticleEntity>
 }
