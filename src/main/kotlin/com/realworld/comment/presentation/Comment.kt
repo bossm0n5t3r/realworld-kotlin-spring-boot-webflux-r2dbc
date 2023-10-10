feed865f41a73c3ccd09a51c3cc8ebcfcafdd2cf
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-10-11 00:15:00 +0900

    feat: Add presentation.Comment

---
+++ src/main/kotlin/com/realworld/comment/presentation/Comment.kt
@@ -0,0 +1,23 @@
+package com.realworld.comment.presentation
+
+import com.realworld.comment.application.dto.CommentDto
+import com.realworld.exception.ErrorCode
+import com.realworld.exception.InvalidRequestException
+import com.realworld.user.presentation.dto.Profile
+import java.time.Instant
+
+data class Comment(
+    val id: Long,
+    val createdAt: Instant,
+    val updatedAt: Instant,
+    val body: String,
+    val author: Profile,
+) {
+    constructor(dto: CommentDto, profile: Profile? = null) : this(
+        id = dto.id ?: throw InvalidRequestException(ErrorCode.COMMENT_NOT_FOUND),
+        createdAt = dto.createdAt,
+        updatedAt = dto.updatedAt,
+        body = dto.body,
+        author = profile ?: Profile(),
+    )
+}
