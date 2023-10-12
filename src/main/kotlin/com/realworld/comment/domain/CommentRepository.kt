362f73ec724bf4dc9c84ec5b0d29ffe5df7a72eb
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-10-12 22:21:00 +0900

    fix: Edit CommentRepository
    - Add findAllByArticleId

--- src/main/kotlin/com/realworld/comment/domain/CommentRepository.kt
+++ src/main/kotlin/com/realworld/comment/domain/CommentRepository.kt
@@ -2,6 +2,9 @@ package com.realworld.comment.domain
 
 import org.springframework.data.r2dbc.repository.R2dbcRepository
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Flux
 
 @Repository
-interface CommentRepository : R2dbcRepository<CommentEntity, Long>
+interface CommentRepository : R2dbcRepository<CommentEntity, Long> {
+    fun findAllByArticleId(articleId: Long): Flux<CommentEntity>
+}
